99d58c9450824ac7eb2ca1670aaf26829f8fd507
AndreiSec <andreisecara01@gmail.com>
2024-01-09 16:21:10 -0500

    Temp commit

---
+++ api/lambdas/get-all-reports/handler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"api/shared/constants"
+	"api/shared/util"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	tableName := os.Getenv(string(constants.ReportTable))
+
+	// Attributes to return
+	projectionExpression := "ReportID, ReportType, Title, City"
+
+	reports, err := util.GetAllItems(tableName, projectionExpression)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Internal Server Error",
+		}, nil
+	}
+
+	responseBody, err := json.Marshal(reports)
+	if err != nil {
+		fmt.Println("Error marshalling response:", err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Internal Server Error",
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseBody),
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
