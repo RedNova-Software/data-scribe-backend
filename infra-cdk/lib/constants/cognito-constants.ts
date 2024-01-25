dd4e18b0c5d58ae96c60503a9780966dfc87454b
AndreiSec <andreisecara01@gmail.com>
2024-01-25 15:50:16 -0500

    Update cognito user pool to new one

--- infra-cdk/lib/constants/cognito-constants.ts
+++ infra-cdk/lib/constants/cognito-constants.ts
@@ -1,2 +1,2 @@
 // This should be updated if the Cognito user pool is ever updated in the frontend cdk
-export const userPoolId = 'us-east-2_iTXm0M333'
+export const userPoolId = "us-east-2_2b9BX9sv5";
