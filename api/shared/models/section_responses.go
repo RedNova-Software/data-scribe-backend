bce0deacd215b1bfb676c714a2388b2d7e13d02f
Andrei Secara <andreisecara01@gmail.com>
2024-02-05 22:40:19 +0000

    Update ChartOutputResponse

--- api/shared/models/section_responses.go
+++ api/shared/models/section_responses.go
@@ -14,6 +14,7 @@ type ChartOutputResponse struct {
 	IndependentColumnAcceptedValues []string
 
 	DependentColumns []OneDimConfigResponse
+	FilterColumns    map[string][]string
 }
 
 type CsvDataResponse struct {
